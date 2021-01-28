a40245e35a421478dc089e1a7a3c5c2ba53637c9
praveen <surathotipraveen31@gmail.com>
2021-01-28 16:03:11 +0530

    update

---
+++ romanint.py
@@ -0,0 +1,11 @@
+s=input("enter the roman nums to convert into integer:")
+def romanInt(s):
+	d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+	ans = 0
+	for i in range(len(s)):
+		if i+1!=len(s) and d[s[i]]<d[s[i+1]]:
+			ans=ans-d[s[i]]
+		else:
+			ans=ans+d[s[i]]
+	return ans
+print(romanInt(s))
