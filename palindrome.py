2c93eda09ac203e229ad1082d51c0d1e85339f39
PraveenSurathoti <75988069+PraveenSurathoti@users.noreply.github.com>
2021-02-11 18:00:53 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+#Palindrome of a string
+
+'''string=int(input(("Enter a string:")))
+if(string==string[::-1]):
+      print("The string is a palindrome")
+else:
+      print("Not a palindrome")'''
+      
+#Palindrome of a integer
+
+
+number=int(input(("Enter a number:")))
+string = str(number)
+if(string==string[::-1]):
+      print("The string is a palindrome")
+else:
+      print("Not a palindrome")
+
+
+
+'''there is no slicing method for integer...for that we can convert integer number to string'''
+
